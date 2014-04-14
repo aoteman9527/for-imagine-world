c68274b4ef2d78b257d4949b883d17dc2bb3411d
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4ff5ac81-6805-e8f9-21f9-4cc465239cc0>
2014-04-14 01:04:30 +0000

    error reporting change

--- SOURCE/v4.2.2/upload/forum.php
+++ SOURCE/v4.2.2/upload/forum.php
@@ -3,7 +3,7 @@
 || #################################################################### ||
 || # vBulletin 4.2.2 - Licence Number VBFRD8D65H
 || # ---------------------------------------------------------------- # ||
-|| # Copyright ©2000-2013 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # Copyright ï¿½2000-2013 vBulletin Solutions Inc. All Rights Reserved. ||
 || # This file may not be redistributed in whole or significant part. # ||
 || # ---------------- VBULLETIN IS NOT FREE SOFTWARE ---------------- # ||
 || # http://www.vbulletin.com | http://www.vbulletin.com/license.html # ||
@@ -11,7 +11,7 @@
 \*======================================================================*/
 
 // ####################### SET PHP ENVIRONMENT ###########################
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE ^ E_WARNING);
 
 // #################### DEFINE IMPORTANT CONSTANTS #######################
 define('THIS_SCRIPT', 'index');
