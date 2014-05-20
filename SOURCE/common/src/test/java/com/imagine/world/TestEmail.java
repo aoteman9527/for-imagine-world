181c283e974ad3efd2aecb1c18dc145471660ea2
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4ff5ac81-6805-e8f9-21f9-4cc465239cc0>
2014-05-20 06:18:27 +0000

    test email

--- SOURCE/common/src/test/java/com/imagine/world/TestEmail.java
+++ SOURCE/common/src/test/java/com/imagine/world/TestEmail.java
@@ -1,14 +1,27 @@
 package com.imagine.world;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import junit.framework.TestCase;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by letuan on 5/20/14.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"myspring-servlet.xml"})
 public class TestEmail extends TestCase {
-    public void testSendMailHtml() throws EmailException {
+
+    @Test
+    public void testSendMailHtml() throws EmailException, IOException {
         String host = "smtp.gmail.com";
         int port = 465;
         String username = "dang.capchemgio@gmail.com";
@@ -18,8 +31,8 @@ public class TestEmail extends TestCase {
         String fromAlias = "CLOUD TV";
         String fromEmail = "dang.capchemgio@gmail.com";
 
-        String body = "asjhajkdh askdjhaskjdhakjshd";
-
+        URL url = Resources.getResource("email.html");
+        String body = Resources.toString(url,Charsets.UTF_8);
         HtmlEmail mail = new HtmlEmail();
 
         mail.setHostName(host);
@@ -29,15 +42,19 @@ public class TestEmail extends TestCase {
         mail.setSSL(useSsl);
         mail.setSmtpPort(port);
         mail.setSslSmtpPort(String.valueOf(port));
-        mail.setCharset("utf-8");
+        mail.setCharset(Charsets.UTF_8.name());
 
         mail.setFrom(fromEmail,fromAlias);
 
         mail.addTo("tuanlhdnl@gmail.com", "");
         mail.setSubject(" effectiveSubject ");
-        mail.setTextMsg(body);
+        mail.setHtmlMsg(body);
 
         mail.send();
+
+    }
+
+    public void testLoadResourceByGuava(){
 
     }
 }
--- SOURCE/common/src/test/java/com/imagine/world/TestPropertiesUtil.java
+++ SOURCE/common/src/test/java/com/imagine/world/TestPropertiesUtil.java
@@ -2,16 +2,21 @@ package com.imagine.world;
 
 import com.imagine.world.config.PropertiesValue;
 import junit.framework.TestCase;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by letuan on 4/21/14.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= "com/imagine/world/myspring-servlet.xml")
 public class TestPropertiesUtil extends TestCase{
 
     public void testLoading(){
-        ApplicationContext context = new ClassPathXmlApplicationContext( "myspring-servlet.xml" );// TOTO : it use file name only
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/imagine/world/myspring-servlet.xml");// TOTO : it use file name only
         PropertiesValue v = (PropertiesValue) context.getBean("propertiesValue");//equivalent to @Autowire
 //        System.out.println(v.VBB_REQUEST_CREATE_CONTEXT);
     }
