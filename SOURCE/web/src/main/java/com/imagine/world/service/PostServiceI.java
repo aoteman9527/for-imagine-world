05327ef39bbcb1b6398001e138b4213c5661e936
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4ff5ac81-6805-e8f9-21f9-4cc465239cc0>
2014-10-24 02:36:12 +0000

    add PostServiceI

---
+++ SOURCE/web/src/main/java/com/imagine/world/service/PostServiceI.java
@@ -0,0 +1,10 @@
+package com.imagine.world.service;
+
+/**
+ * Created by tuan on 10/23/14.
+ */
+public interface PostServiceI {
+    public void postNew();
+    public void deletePost();
+    public void getTopic();
+}
