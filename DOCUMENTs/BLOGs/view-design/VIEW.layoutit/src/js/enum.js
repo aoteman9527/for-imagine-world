81ae442d3b65d95fb280bfd86850f1110318e06a
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4ff5ac81-6805-e8f9-21f9-4cc465239cc0>
2014-11-14 09:35:35 +0000

    blog source code. may be moved another folder

---
+++ DOCUMENTs/BLOGs/view-design/VIEW.layoutit/src/js/enum.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tuanlhd on 11/14/14.
+ */
+EnumRedirectPage = {
+    HOME_PAGE:"",
+    FORUM_PAGE:"indexForum",
+    TOPIC_PAGE:"indexTopic"
+}
---
+++ DOCUMENTs/BLOGs/view-design/VIEW.layoutit/src/js/scripts.js
@@ -0,0 +1,75 @@
+Function.prototype.inheritsFrom = function( parentClassOrObject ){
+    if ( parentClassOrObject.constructor == Function )
+    {
+        //Normal Inheritance
+        this.prototype = new parentClassOrObject;
+        this.prototype.constructor = this;
+        this.prototype.parent = parentClassOrObject.prototype;
+    }
+    else
+    {
+        //Pure Virtual Inheritance
+        this.prototype = parentClassOrObject;
+        this.prototype.constructor = this;
+        this.prototype.parent = parentClassOrObject;
+    }
+    return this;
+}
+
+var include = function(insideElement,url){
+    jQuery.get(url,function(data){
+        jQuery(insideElement).replaceWith(data);
+
+    });
+}
+function BController () {
+    PARAM_REDIRECT = "redirect"
+
+    this.urlParam=function(name){
+        var results = new RegExp('[\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+        return results !=null ? (results[1] || 0):0;
+    }
+
+    this.onLoad=function(){
+        var pageDirect = this.urlParam(PARAM_REDIRECT);
+        switch (pageDirect){
+            case EnumRedirectPage.HOME_PAGE:
+
+                break;
+            case EnumRedirectPage.FORUM_PAGE:
+
+                break;
+            case EnumRedirectPage.TOPIC_PAGE:
+
+                break;
+            default :
+                jQuery("body").html(this.getViewHtml());
+                break;
+        }
+    }
+
+    this.onMenu1=function(){
+
+    }
+
+    this.loadTopic = function(topicId,callback){
+
+    }
+    /**
+     * You must implement this function. cause it is getting view of each Page.
+     */
+    this.getViewHtml = function(){
+        throw new Error("this function is not support");
+    }
+};
+
+HomeController.inheritsFrom(BController);
+function HomeController () {
+    this.getViewHtml = function(){
+        return "<img id=\"indexHome\" onload=\"include(this,this.id+'.html')\" src=\"img/apple-touch-icon-144-precomposed.png\" >";
+    }
+}
+
+
+var controller = new HomeController();
+
