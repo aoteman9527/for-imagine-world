65a672e9875b541a478ab95a36fcdabe0c98f932
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4ff5ac81-6805-e8f9-21f9-4cc465239cc0>
2014-12-10 17:13:42 +0000

    add PostServiceI

--- SOURCE/crawler/src/main/java/com/imagine/world/crawler/TruyenTranhTuanMain.java
+++ SOURCE/crawler/src/main/java/com/imagine/world/crawler/TruyenTranhTuanMain.java
@@ -11,11 +11,19 @@ public class TruyenTranhTuanMain {
     public static void main(String[] arg) throws InterruptedException {
         HomePage homePage = new HomePage("http://truyentranhtuan.com/");
         homePage.start();
-
-        int shutdownTime = 2*60*1000;
-        Thread.sleep(shutdownTime);
-        System.out.println("Shutdown system ");
-        Page.getThreadPoolExecutor().shutdown();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int shutdownTime = 2*60*1000;
+                try {
+                    Thread.sleep(shutdownTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Shutdown system ");
+                Page.getThreadPoolExecutor().shutdown();
+            }
+        }).start();
 
         while(!Page.getThreadPoolExecutor().isTerminated()){
             Thread.sleep(4000);
