c204afc5077c65bbd93aed1a5137c1606e964d40
tuanlhdnl <tuanlhdnl@4ff5ac81-6805-e8f9-21f9-4cc465239cc0>
2014-12-29 04:10:14 +0000

    update header

--- wordpress/wp-content/themes/alpha-trinity/header.php
+++ wordpress/wp-content/themes/alpha-trinity/header.php
@@ -1,28 +1,36 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>" />           
-    <meta name="viewport" content="width=device-width, initial-scale=1">   
+<!--TUAN CUSTOM META-->
+    <link rel="author" href="https://plus.google.com/+LeeTu%E1%BA%A5nl%C3%A9/about">
+    <meta name="description" content="<?php $post = get_post(get_the_ID()); echo strip_tags(substr($post->post_title,0 , 500)); ?>">
+    <!--TUAN CUSTOM META-->
+
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo(‘name’); ?><?php wp_title(); ?></title>
 
     <meta http-equiv=”Content-Type” content=”<?php bloginfo(‘html_type’); ?>; charset=<?php bloginfo(‘charset’); ?>” />
-<meta name="description" content="<?php $post = get_post(get_the_ID()); echo strip_tags(substr($post->post_title,0 , 500)); ?>">
-    <link rel="profile" href="http://gmpg.org/xfn/11" />        
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />     
+
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <!--[if lt IE 9]>
-		<script src="<?php echo get_template_directory_uri(); ?>/assets/html5shiv.js" type="text/javascript"></script>
-	<![endif]-->     
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/html5shiv.js" type="text/javascript"></script>
+    <![endif]-->
     <?php wp_head(); ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-57161245-3', 'auto');
-  ga('send', 'pageview');
+    <!--GOOGLE ANALYTICs-->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-</script>
+        ga('create', 'UA-57161245-3', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
 </head>
 <body <?php body_class(); ?>>
 	<div class="container">
